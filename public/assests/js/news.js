01c40ed16d1283907ea697361af4c606a3a9837e
Nigel <neastern101@yahoo.com>
2019-07-19 18:30:47 -0500

    front-end update

---
+++ public/assests/js/news.js
@@ -0,0 +1,35 @@
+$(document).on("click", "#scrape", function () {
+    console.log('scrape');
+    $.get("/scrape").then(function (data) {
+        location.reload();
+    });
+});
+
+$(document).on("click", "#submitButton", function (event) {
+    event.preventDefault();
+    let requestBody = {
+        title: $("#title")
+            .val()
+            .trim(),
+        body: $("#body")
+            .val()
+            .trim()
+    };
+    $.post(location.href, requestBody).then(function (data) {
+        location.reload();
+    });
+});
+
+
+$(document).on("click", ".js-deleteComment", function (event) {
+    event.preventDefault();
+    let commentID = $(this).attr("data-id");
+    $.ajax({
+        url: location.href + "/" + commentID,
+        type: 'DELETE',
+        success: function (result) {
+            location.reload();
+        }
+    });
+});
+
