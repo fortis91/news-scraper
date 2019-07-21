29d3bc7685b886426d38d4ca9d28a71908a7cda1
Nigel <neastern101@yahoo.com>
2019-07-21 14:34:11 -0500

    update comment page

--- public/assets/js/news.js
+++ public/assets/js/news.js
@@ -6,31 +6,31 @@ $(document).on("click", "#scrape", function () {
     });
 });
 
-$(document).on("click", "#submitButton", function (event) {
-    event.preventDefault();
-    let requestBody = {
-        title: $("#title")
-            .val()
-            .trim(),
-        body: $("#body")
-            .val()
-            .trim()
-    };
-    $.post(location.href, requestBody).then(function (data) {
-        location.reload();
-    });
-});
+// $(document).on("click", "#submitButton", function (event) {
+//     event.preventDefault();
+//     let requestBody = {
+//         title: $("#title")
+//             .val()
+//             .trim(),
+//         body: $("#body")
+//             .val()
+//             .trim()
+//     };
+//     $.post(location.href, requestBody).then(function (data) {
+//         location.reload();
+//     });
+// });
 
 
-$(document).on("click", ".js-deleteComment", function (event) {
-    event.preventDefault();
-    let commentID = $(this).attr("data-id");
-    $.ajax({
-        url: location.href + "/" + commentID,
-        type: 'DELETE',
-        success: function (result) {
-            location.reload();
-        }
-    });
-});
+// $(document).on("click", ".js-deleteComment", function (event) {
+//     event.preventDefault();
+//     let commentID = $(this).attr("data-id");
+//     $.ajax({
+//         url: location.href + "/" + commentID,
+//         type: 'DELETE',
+//         success: function (result) {
+//             location.reload();
+//         }
+//     });
+// });
 
